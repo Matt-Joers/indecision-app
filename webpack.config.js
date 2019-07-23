59aabb0122248f10fa716bb356ab4a3254ef48c6
Matt Joers <matthew.joers@froedtert.com>
2019-07-23 12:57:29 -0500

    initial webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// entry point of app - app.js in source folder
+// where to output the bundle file -> webpack.config.js file
+// expose an object to another file
+// webpack.js.org
+const path = require('path');
+console.log(path.join(__dirname, 'public'));
+
+module.exports = {
+  entry: './public/src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  }
+};
